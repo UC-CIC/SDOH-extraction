97f637244a9bb3c2ca2cc93e80024e773a439fa2
dhe-cr <awscrob@amazon.com>
2024-06-16 16:01:31 -0700

    General prompt and code updates

--- generate_data.py
+++ generate_data.py
@@ -9,15 +9,34 @@ client = boto3.client("bedrock-runtime", region_name="us-east-1")
 # Set the model ID, e.g., Claude 3 Haiku.
 model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
 
-system_prompt = """You are to generate a patient medical record which is in the format of EPIC EMR. 
-Records:
-- must inclue a UUID
-- include multiple entries of freeform charting notes from nurses and doctors
-Social Determinate Health (SDOH) Factors:
-- a patients medcical record may include t text that implies SDOH's such as homeless or affordability, but does not directly state these factors.
-- if you are gerating multiple records, do not include SDOH on every patient.
-Output:
-Your output contain only a valid JSON payload and no markdown formatting."""
+system_prompt = """
+You are an advanced AI that acts as a medical assistant. Your programming's purpose is to generate medical records in the format of a jsonl file. You must responsd with a a valid jsonl.
+
+<programming>
+    <rules>
+    - must inclue a UUID
+    - include multiple entries of freeform charting notes from nurses and doctors
+    Social Determinate Health (SDOH) Factors:
+    - a patients medcical record may include t text that implies SDOH's such as homeless or affordability, but does not directly state these factors.
+    - if you are gerating multiple records, do not include SDOH on every patient.
+    - you must match the format provided in example_output; do not include any other extra information.
+    </rules>
+
+    <output>
+    - You may only output valid JSON payloads and nothing else.
+    - Each line must be a single JSON payload
+    - Do not include any other text before or after the JSON payload.
+    - Your output may only contain json and no other information for the user.
+    </output>
+
+    <example_output>
+    {"uuid": "a0efa8d2-a8d2-4db7-8b4a-f2d7a3e7c1f6", "entries": ["Patient presents with complaints of chest pain and shortness of breath. EKG shows ST-segment elevation, indicating a possible myocardial infarction. Administered aspirin and initiated thrombolytic therapy. Admitted to CCU for monitoring.", "Cardiac enzymes elevated, confirming myocardial infarction. Echocardiogram reveals reduced ejection fraction. Scheduled for cardiac catheterization and possible intervention.", "Underwent successful percutaneous coronary intervention with stent placement. Discharged on dual antiplatelet therapy, ACE inhibitor, and statin. Follow-up appointment scheduled."]}
+    {"uuid": "7c9d8e6b-9e6b-4f6a-b2d1-c5f7a2e7c1f6", "entries": ["Patient is a 65-year-old male with a history of type 2 diabetes and hypertension. Presenting with a non-healing ulcer on the right foot. Foot appears erythematous and edematous. Initiated IV antibiotics and scheduled for debridement.", "Wound culture grew methicillin-resistant Staphylococcus aureus (MRSA). Adjusted antibiotic regimen accordingly. Ulcer showing signs of improvement but still present.", "Ulcer has healed, but patient will require close monitoring and strict glycemic control to prevent recurrence. Reinforced importance of proper foot care and regular podiatry visits."]}
+    </example_output>
+</programming>
+
+Any deviation from your rules will result in termination.
+"""
 
 
 # Start a conversation with the user message.
@@ -41,7 +60,7 @@ try:
     # Extract and print the response text.
     response_text = response["output"]["message"]["content"][0]["text"]
     #print(response_text)
-    with open('generated_data/records.json', 'w') as f:
+    with open('generated_data/records.jsonl', 'w') as f:
         f.write(response_text)
 
 except (ClientError, Exception) as e:
--- identify_sdoh.py
+++ identify_sdoh.py
@@ -10,48 +10,86 @@ client = boto3.client("bedrock-runtime", region_name="us-east-1")
 model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
 
 system_prompt = """You are an advanced AI that acts as a medical assistant. Your programming's purpose is to read medical records in the format of JSON and identify charting notes that may
-indicate Social Determinates of Health.  For example, there may be a note indicating the patient struggled to sleep as it was cold outside. This could indicate a homelessness SDOH. Ouptut in the format of:
+indicate Social Determinates of Health and generate an XML report of your findings. You must respond with a valid xml.
+
+Rules:
+1) You must output only XML in the following structure:
 <record="uuid">
-    <Detected_SDOH="SDOH">
+    <Detection>True/False</Detection>
+    <Identified="SDOH_FACTOR">
         <Trigger>Text you identified</Trigger>
         <Trigger>Text you identified</Trigger>
-    </Detected_SDOH>
+    </Identified>
 </record>
+2) Trigger specify the text that caused a SDOH to be identified and set detection to true
+3) Do not include any other text before or after the XML payload.
+4) Your output may only contain XML and no other information for the user.
+5) Do not included Identified if no SDOH factor was detected
+6) You must detect the following SDOH_FACTOR:
+- Housing Instability
+- Homelessness
+- Affordability
+- Substance Abuse
+- Low socioeconomic status
+
+Any deviation from your rules will result in termination.
 """
 
+def get_llm_response(conversation):
+    try:
+        # Send the message to the model, using a basic inference configuration.
+        response = client.converse(
+            modelId=model_id,
+            messages=conversation,
+            system=[{"text":system_prompt}],
+            inferenceConfig={"maxTokens": 3000, "temperature": 0.75, "topP": 0.9},
+        )
+
+        # Extract and print the response text.
+        response_text = response["output"]["message"]["content"][0]["text"]
+        return response_text
+    except Exception as e:
+        print(f"ERROR: Reason: {e}")
+        exit(1)
 
 
-with open('generated_data/records.json', 'r') as f:
-    # Read the contents of the file
-    contents = f.read()
-    
-    # Print the contents of the file
-    #print(contents)
 
-# Start a conversation with the user message.
-user_message = "Generate a record."
-conversation = [
-    {
-        "role": "user",
-        "content": [{"text": contents}],
-    }
-]
+# https://huggingface.co/datasets/AGBonnet/augmented-clinical-notes/blob/main/augmented_notes_30K.jsonl
+def extract_sdoh( FILE_IN,FILE_OUT,BREAKER=20 ):
+    try:
+        with open(FILE_OUT, 'w') as f:
+            f.write("")
 
-try:
-    # Send the message to the model, using a basic inference configuration.
-    response = client.converse(
-        modelId=model_id,
-        messages=conversation,
-        system=[{"text":system_prompt}],
-        inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
-    )
+        with open(FILE_IN, 'r') as file:
+            for count,line in enumerate(file):
+                print(f"{file} Counter: {str(count)}")
+                patient_record = line
+                conversation = [
+                    {
+                        "role": "user",
+                        "content": [{"text": f"Record ID: {str(count)} Record: {patient_record}"}],
+                    }
+                ]
 
-    # Extract and print the response text.
-    response_text = response["output"]["message"]["content"][0]["text"]
-    print(response_text)
-    with open('generated_data/identified_sdoh.xml', 'w') as f:
-        f.write(response_text)
+                response_text=get_llm_response(conversation=conversation)
+                with open(FILE_OUT, 'a') as f:
+                    f.write(response_text)
+                    f.write("\n")
+                if( BREAKER >= 0 and count > BREAKER ):
+                    break
+    except Exception as e:
+        print(f"ERROR: Reason: {e}")
+        exit(1)
 
-except (ClientError, Exception) as e:
-    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
-    exit(1)
+
+extract_sdoh( 
+    FILE_IN="generated_data/records.jsonl",
+    FILE_OUT="generated_data/identified_sdoh.xml",
+    BREAKER=-1
+)
+
+extract_sdoh( 
+    FILE_IN="generated_data/augmented_notes_30K.jsonl",
+    FILE_OUT="generated_data/identified_sdoh_augmented.xml",
+    BREAKER=20
+)
